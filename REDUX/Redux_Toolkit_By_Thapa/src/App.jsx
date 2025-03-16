2675aabed2fd1f1d5e26f92044504842a9b451f7
NoorAlamSabbagh <alamrangrej787@gmail.com>
2025-03-16 22:43:22 +0530

    Redux ToolKit

--- REDUX/Redux_Toolkit_By_Thapa/src/App.jsx
+++ REDUX/Redux_Toolkit_By_Thapa/src/App.jsx
@@ -1,6 +1,8 @@
+import { useSelector } from "react-redux";
 import { Todo } from "./component/Todo";
 
 const App = () => {
+  // console.log("UseSelector", useSelector((state) =>state?.taskReducer.task))
     return (
         <>
         <Todo/>
